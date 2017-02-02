a3068c733c331e3fc0a37c762a0f48675395282a
Logan Medina <logan.medina@gmail.com>
2017-02-01 18:43:49 -0800

    fixed border

--- game2/assets/js/app.js
+++ game2/assets/js/app.js
@@ -1,8 +1,8 @@
 // $('#play').click(begin);
 //
 // $('#clear').click(endGame);
-
-
+var y = $('#game-board').height();
+var x = $('#game-board').width()-85;
 //
 $(document).keydown(move);
 // //
@@ -45,37 +45,52 @@ $(document).keydown(move);
 // // }
 //
 function move(key){
-  if (key.which === 186 && $('#playerA').position().right != 0){
+  // Player A Right - Done
+  if (key.which === 186 && (x-$('#playerA').position().left)>0){
     var currentX = $('#playerA').position().left;
-    $('#playerA').css('left',currentX+15);
+    $('#playerA').css('left',currentX+10);
     $('#playerA').css('rotate', -90);
-  } else if (key.which === 75 && $('#playerA').position().left != 0){
+  }
+    // Player A Left - Done
+    else if (key.which === 75 && $('#playerA').position().left != 0){
     var currentX = $('#playerA').position().left;
-    $('#playerA').css('left',currentX-15);
+    $('#playerA').css('left',currentX-10);
     $('#playerA').css('rotate', 90);
-  } else if (key.which === 76 && $('#playerA').position().bottom != 0){
+  }
+    // Player A Down - Done
+    else if (key.which === 76 && (y-$('#playerA').position().top) > 210){
     var currentY = $('#playerA').position().top;
-    $('#playerA').css('top', currentY+15);
+    $('#playerA').css('top', currentY+10);
     $('#playerA').css('rotate', -360);
-  } else if (key.which === 79 && $('#playerA').position().top != 0){
+  }
+    // Player A Up - Done
+    else if (key.which === 79 && $('#playerA').position().top > 0){
     var currentY = $('#playerA').position().top;
-    $('#playerA').css('top', currentY-15);
+    $('#playerA').css('top', currentY-10);
     $('#playerA').css('rotate', 180);
-  } else if (key.which === 68 && $('#playerB').position().right != 0){
+  }
+    // Player B Right - Done
+    else if (key.which === 68 && (x-$('#playerB').position().left)>0){
     var currentX = $('#playerB').position().left;
-    $('#playerB').css('left',currentX+15);
+    $('#playerB').css('left',currentX+10);
     $('#playerB').css('rotate', -90);
-  } else if (key.which === 65 && $('#playerB').position().left != 0){
+  }
+    // Player B Left - Done
+    else if (key.which === 65 && $('#playerB').position().left != 0){
     var currentX = $('#playerB').position().left;
-    $('#playerB').css('left',currentX-15);
+    $('#playerB').css('left',currentX-10);
     $('#playerB').css('rotate', 90);
-  } else if (key.which === 83 && $('#playerB').position().bottom != 0){
+  }
+    // Player B Down - Done
+    else if (key.which === 83 && (y-$('#playerB').position().top) > 210){
     var currentY = $('#playerB').position().top;
-    $('#playerB').css('top', currentY+15);
+    $('#playerB').css('top', currentY+10);
     $('#playerB').css('rotate', -360);
-  } else if (key.which === 87 && $('#playerB').position().top != 0){
+  }
+    // Player B Up - Done
+    else if (key.which === 87 && $('#playerB').position().top > 0){
     var currentY = $('#playerB').position().top;
-    $('#playerB').css('top', currentY-15);
+    $('#playerB').css('top', currentY-10);
     $('#playerB').css('rotate', 180);
   }
 }
