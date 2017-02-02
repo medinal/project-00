b7d19e998baa2a89d102075bdec3f06d3792e108
Logan Medina <logan.medina@gmail.com>
2017-02-01 22:38:17 -0800

    got collision detection and location of starting pieces set

--- game2/assets/js/app.js
+++ game2/assets/js/app.js
@@ -1,43 +1,39 @@
-// $('#play').click(begin);
-//
-// $('#clear').click(endGame);
+$('#play').click(begin);
+
+$('#clear').click(endGame);
+
 var y = $('#game-board').height();
 var x = $('#game-board').width()-85;
-//
-$(document).keydown(move);
-// //
-// // function begin(){
-// //   createBoard();
-// //   $('#play').attr("disabled", "disabled");
-// //   $('.notification').text(3);
-// //   setTimeout(countdown,1000);
-// // }
-// //
-// //
-// // function countdown(){
-// //   var countdownTime = parseInt($('.notification').text());
-// //   if (parseInt($('.notification').text()) === 1){
-// //     $('.notification').text("GO!");
-// //     countdown();
-// //   } else if (parseInt($('.notification').text()) >1){
-// //     countdownTime--;
-// //     console.log($('.notification').text());
-// //     $('.notification').text(countdownTime);
-// //     setTimeout(countdown,1000);
-// //   } else if ($('.notification').text()==="GO!"){
-// //     console.log("success");
-// //     $(document).keydown(move);
-// //   };
-// // }
-// //
-// // function endGame(){
-// //   $('#play').removeAttr("disabled");
-// //   $('#playerA').attr('class', 'col-xs-1');
-// //   $('#playerA').attr('data-loc', '0');
-// //   $('#playerB').attr('class', 'col-xs-1');
-// //   $('#playerB').attr('data-loc', '0');
-// //   $(document).off('keydown', move);
-// // }
+
+function begin(){
+  $('#play').attr("disabled", "disabled");
+  $('.notification').text(3);
+  setTimeout(countdown,1000);
+}
+
+function countdown(){
+  var countdownTime = parseInt($('.notification').text());
+  if (parseInt($('.notification').text()) === 1){
+    $('.notification').text("GO!");
+    countdown();
+  } else if (parseInt($('.notification').text()) >1){
+    countdownTime--;
+    console.log($('.notification').text());
+    $('.notification').text(countdownTime);
+    setTimeout(countdown,1000);
+  } else if ($('.notification').text()==="GO!"){
+    console.log("success");
+    $(document).keydown(move);
+    timing();
+  };
+}
+
+function endGame(){
+  $('#play').removeAttr("disabled");
+  $(document).off('keydown', move);
+  total = 0;
+  timing();
+}
 // //
 // // function createBoard(){
 // //   $('#playerA').attr('class', 'col-xs-1');
@@ -50,57 +46,107 @@ function move(key){
     var currentX = $('#playerA').position().left;
     $('#playerA').css('left',currentX+10);
     $('#playerA').css('rotate', -90);
+    collision()
   }
     // Player A Left - Done
-    else if (key.which === 75 && $('#playerA').position().left != 0){
+    else if (key.which === 75 && (x-$('#playerA').position().left) > 0){
     var currentX = $('#playerA').position().left;
     $('#playerA').css('left',currentX-10);
     $('#playerA').css('rotate', 90);
+    collision()
   }
     // Player A Down - Done
     else if (key.which === 76 && (y-$('#playerA').position().top) > 210){
     var currentY = $('#playerA').position().top;
     $('#playerA').css('top', currentY+10);
     $('#playerA').css('rotate', -360);
+    collision()
   }
     // Player A Up - Done
     else if (key.which === 79 && $('#playerA').position().top > 0){
     var currentY = $('#playerA').position().top;
     $('#playerA').css('top', currentY-10);
     $('#playerA').css('rotate', 180);
+    collision()
   }
     // Player B Right - Done
     else if (key.which === 68 && (x-$('#playerB').position().left)>0){
     var currentX = $('#playerB').position().left;
     $('#playerB').css('left',currentX+10);
     $('#playerB').css('rotate', -90);
+    collision()
   }
     // Player B Left - Done
-    else if (key.which === 65 && $('#playerB').position().left != 0){
+    else if (key.which === 65 && $('#playerB').position().left > 0){
     var currentX = $('#playerB').position().left;
     $('#playerB').css('left',currentX-10);
     $('#playerB').css('rotate', 90);
+    collision()
   }
     // Player B Down - Done
     else if (key.which === 83 && (y-$('#playerB').position().top) > 210){
     var currentY = $('#playerB').position().top;
     $('#playerB').css('top', currentY+10);
     $('#playerB').css('rotate', -360);
+    collision()
   }
     // Player B Up - Done
     else if (key.which === 87 && $('#playerB').position().top > 0){
     var currentY = $('#playerB').position().top;
     $('#playerB').css('top', currentY-10);
     $('#playerB').css('rotate', 180);
+    collision()
   }
 }
-//
-// // function checkWin(){
-// //   if(parseInt($('#playerA').attr('data-loc'))===11){
-// //     $('.notification').text("Player 1 Wins!")
-// //     $(document).off('keydown', move);
-// //   } else if(parseInt($('#playerB').attr('data-loc'))===11){
-// //     $('.notification').text("Player 2 Wins!")
-// //     $(document).off('keydown', move);
-// //   }
-// // }
+
+function collision(){
+  if ($('#playerA').position().left < $('#playerB').position().left + 50 &&
+     $('#playerA').position().left + 50 > $('#playerB').position().left &&
+     $('#playerA').position().top < $('#playerB').position().top + 50 &&
+     50 + $('#playerA').position().top > $('#playerB').position().top) {
+     endGame();
+  }
+}
+
+var startTime; // undefined
+
+// track the most recent "end" time
+var endTime; // undefined
+
+// a boolean to keep track of whether the timer is on or off
+var counting = false;
+
+// variable used for continuous time update while counting
+var repeatDisplay;
+
+// stores total time counted so far
+var total = 0;
+
+function timing(){
+    if (!counting){
+      // we weren't counting,
+      // so this click should start the timer...
+      counting = true;
+      // ... and record the start time...
+      startTime = Date.now();
+      // ... and start updating the display every 100 milliseconds
+      repeatDisplay = setInterval(showCurrentTotal, 100);
+    } else {
+      // we were already counting,
+      // so this click should stop the timer...
+      counting = false;
+      // ... and stop updating the display automatically...
+      clearInterval(repeatDisplay);
+      // ... and update and display the total
+      endTime = (Date.now() - startTime)/1000;
+      total = total + endTime;
+      $("#total-time").text( total + " seconds" );
+  }
+}
+
+function showCurrentTotal(){
+  // calculate time since we last pressed start
+  var elapsedTime = (Date.now() - startTime)/1000;
+  // display the total time so far
+  $("#total-time").text( (total + elapsedTime) + " seconds" );
+}
