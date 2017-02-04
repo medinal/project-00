2f473dc7ff56c4a45ae7d6c66e892b0ee2000152
Logan Medina <logan.medina@gmail.com>
2017-02-03 16:25:09 -0800

    collision detection with asteroids

--- game2/assets/js/app.js
+++ game2/assets/js/app.js
@@ -25,8 +25,8 @@ function countdown(){
   } else if ($('.notification').text()==="GO!"){
     $(document).keydown(move);
     timing();
-    var makeAsteroids = setInterval(createAsteroid, Math.random() * (4000) + 500);
-    var asteroidCollisionDetection = setTimeout(asteroidCollision,10);
+    var makeAsteroids = setInterval(createAsteroid, Math.random() * (3000)+500);
+    var asteroidCollisionDetection = setInterval(asteroidCollision,20);
   };
 }
 
@@ -44,12 +44,12 @@ function endGame(){
   startTime=0;
   endTime=0;
   repeatDisplay=0;
-  $('#playerA').css('left', "");
-  $('#playerA').css('top', "");
-  $('#playerA').css('rotate', "");
-  $('#playerB').css('top', "");
-  $('#playerB').css('rotate', "");
-  $('#playerB').css('left', "");
+  $('#playerA').css('left', "0");
+  $('#playerA').css('top', "300");
+  $('#playerA').css('rotate', "-90");
+  $('#playerB').css('top', "400");
+  $('#playerB').css('rotate', "-90");
+  $('#playerB').css('left', "0");
   $('.notification').text("");
 }
 
@@ -65,12 +65,12 @@ function reset(){
   startTime=0;
   endTime=0;
   repeatDisplay=0;
-  $('#playerA').css('left', "");
-  $('#playerA').css('top', "");
+  $('#playerA').css('left', "0");
+  $('#playerA').css('top', "300");
   $('#playerA').css('rotate', "-90");
-  $('#playerB').css('top', "");
+  $('#playerB').css('top', "400");
   $('#playerB').css('rotate', "-90");
-  $('#playerB').css('left', "");
+  $('#playerB').css('left', "0");
   $('.notification').text("");
 }
 
@@ -79,56 +79,56 @@ function move(key){
   // Player A Right - Done
   if (key.which === 186 && (x-$('#playerA').position().left)>0){
     var currentX = $('#playerA').position().left;
-    $('#playerA').css('left',currentX+20);
+    $('#playerA').animate({left: "+=30"}, "fast");
     $('#playerA').css('rotate', -90);
     collision()
   }
     // Player A Left - Done
     else if (key.which === 75 && ($('#playerA').position().left) > 0){
     var currentX = $('#playerA').position().left;
-    $('#playerA').css('left',currentX-20);
+    $('#playerA').animate({left: "-=30"}, "fast");
     $('#playerA').css('rotate', 90);
     collision()
   }
     // Player A Down - Done
     else if (key.which === 76 && (y-$('#playerA').position().top) > 210){
     var currentY = $('#playerA').position().top;
-    $('#playerA').css('top', currentY+20);
+    $('#playerA').animate({top: "+=30"}, "fast");
     $('#playerA').css('rotate', -360);
     collision()
   }
     // Player A Up - Done
     else if (key.which === 79 && $('#playerA').position().top > 0){
     var currentY = $('#playerA').position().top;
-    $('#playerA').css('top', currentY-20);
+    $('#playerA').animate({top: "-=30"}, "fast");
     $('#playerA').css('rotate', 180);
     collision()
   }
     // Player B Right - Done
     else if (key.which === 68 && (x-$('#playerB').position().left)>0){
     var currentX = $('#playerB').position().left;
-    $('#playerB').css('left',currentX+20);
+    $('#playerB').animate({left: "+=30"}, "fast");
     $('#playerB').css('rotate', -90);
     collision()
   }
     // Player B Left - Done
     else if (key.which === 65 && $('#playerB').position().left > 0){
     var currentX = $('#playerB').position().left;
-    $('#playerB').css('left',currentX-20);
+    $('#playerB').animate({left: "-=30"}, "fast");
     $('#playerB').css('rotate', 90);
     collision()
   }
     // Player B Down - Done
     else if (key.which === 83 && (y-$('#playerB').position().top) > 210){
     var currentY = $('#playerB').position().top;
-    $('#playerB').css('top', currentY+20);
+    $('#playerB').animate({top: "+=30"}, "fast");
     $('#playerB').css('rotate', -360);
     collision()
   }
     // Player B Up - Done
     else if (key.which === 87 && $('#playerB').position().top > 0){
     var currentY = $('#playerB').position().top;
-    $('#playerB').css('top', currentY-20);
+    $('#playerB').animate({top: "-=30"}, "fast");
     $('#playerB').css('rotate', 180);
     collision()
   }
@@ -170,29 +170,36 @@ function showCurrentTotal(){
 }
 
 // Asteroid Stuff
-var asteroidNum=0;
+var asteroidNum = 0;
 function createAsteroid(){
   if (asteroidNum === 10){
     asteroidNum=0;
     $(".asteroid").remove();
   } else {
-    $("#game-board").append('<img id="asteroid' + asteroidNum + '" class="asteroid img-responsive" data-xval="' + `${x}` +  '" src="assets/imgs/asteroid.png">')
-    $('#asteroid' + asteroidNum + '').css('left', x);
-    $('#asteroid' + asteroidNum + '').css('top', (Math.random()*y));
-    $('#asteroid' + asteroidNum + '').animate({
-      left: -x,
-    }, Math.random()*10000);
+    $("#game-board").append(`<img id="asteroid${asteroidNum}" class="asteroid img-responsive" src="assets/imgs/asteroid.png">`);
+    $(`#asteroid${asteroidNum}`).css('left', x);
+    $(`#asteroid${asteroidNum}`).css('top', (Math.random()*y)-75);
+    $(`#asteroid${asteroidNum}`).animate({'left': -x}, Math.random()*10000);
     asteroidNum++;
   };
 };
 
 function asteroidCollision(){
-  for (i=0; i<asteroidNum; i++){
-    if ($('#playerA').position().left < $('#asteroid' + asteroidNum + '').position().left + 200 &&
-       $('#playerA').position().left + 200 > $('#asteroid' + asteroidNum + '').position().left &&
-       $('#playerA').position().top < $('#asteroid' + asteroidNum + '').position().top + 75 &&
-       75 + $('#playerA').position().top > $('#asteroid' + asteroidNum + '').position().top) {
-       console.log("collision");
+  if (asteroidNum != 0){
+    for (i=0; i<15; i++){
+      if ($(`#asteroid${i}`).position().left < $('#playerA').position().left + 60 &&
+         $(`#asteroid${i}`).position().left + 75 > $('#playerA').position().left &&
+         $(`#asteroid${i}`).position().top < $('#playerA').position().top + 60 &&
+         60 + $(`#asteroid${i}`).position().top > $('#playerA').position().top) {
+           endGame();
+           $("#total-time").text("Player 1 got hit!");
+         } else if ($(`#asteroid${i}`).position().left < $('#playerB').position().left + 60 &&
+         $(`#asteroid${i}`).position().left + 75 > $('#playerB').position().left &&
+         $(`#asteroid${i}`).position().top < $('#playerB').position().top + 60 &&
+         60 + $(`#asteroid${i}`).position().top > $('#playerB').position().top) {
+           $("#total-time").text("Player 2 got hit!");
+           endGame();
+        }
+      }
     }
   }
-}
